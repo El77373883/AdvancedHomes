61f742e9f665ecaa9e74330c2ae86cf9615795a6
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 21:46:43 -0600

    Update Main.java

--- src/main/java/com/adrian/advancedhomes/Main.java
+++ src/main/java/com/adrian/advancedhomes/Main.java
@@ -3,13 +3,21 @@ package com.adrian.advancedhomes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    private static Main instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         getLogger().info("AdvancedHomes habilitado!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("AdvancedHomes deshabilitado!");
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 }
