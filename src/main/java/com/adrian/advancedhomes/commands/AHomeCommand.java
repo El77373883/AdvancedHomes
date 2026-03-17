97faa8922a04a70eb2f1d9cc13bd999785a976c3
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 21:48:53 -0600

    Update AHomeCommand.java

--- src/main/java/com/adrian/advancedhomes/commands/AHomeCommand.java
+++ src/main/java/com/adrian/advancedhomes/commands/AHomeCommand.java
@@ -18,28 +18,30 @@ import java.util.UUID;
 
 public class AHomeCommand implements CommandExecutor, Listener {
 
-    private static HashMap<UUID, Boolean> awaitingName = new HashMap<>();
+    private static final HashMap<UUID, Boolean> awaitingName = new HashMap<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) return true;
         Player player = (Player) sender;
 
+        // Abrir menú si no hay args
         if (args.length == 0) {
             UserHomeGUI.open(player);
             return true;
         }
 
-        switch(args[0].toLowerCase()) {
+        switch (args[0].toLowerCase()) {
             case "create":
                 if (!HomeManager.canSetHome(player)) {
                     player.sendMessage("§cHas alcanzado tu límite de homes!");
-                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO,1f,1f);
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 1f);
                     return true;
                 }
                 awaitingName.put(player.getUniqueId(), true);
                 player.sendMessage("§aEscribe el nombre de tu home en el chat...");
                 break;
+
             case "remove":
                 if (args.length < 2) return false;
                 String homeRemove = args[1];
@@ -49,9 +51,10 @@ public class AHomeCommand implements CommandExecutor, Listener {
                 }
                 HomeManager.setHome(player, homeRemove, null);
                 player.sendMessage("§aHome '" + homeRemove + "' eliminado!");
-                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP,1f,1f);
-                player.spawnParticle(Particle.VILLAGER_HAPPY, player.getLocation(),20,0.5,1,0.5);
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f);
+                player.spawnParticle(Particle.VILLAGER_HAPPY, player.getLocation(), 20, 0.5, 1, 0.5);
                 break;
+
             case "tp":
                 if (args.length < 2) return false;
                 String homeTp = args[1];
@@ -61,19 +64,20 @@ public class AHomeCommand implements CommandExecutor, Listener {
                 }
                 if (!CooldownManager.canTeleport(player)) {
                     player.sendMessage("§cEspera " + CooldownManager.getRemaining(player) + "s antes de teletransportarte!");
-                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO,1f,1f);
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 1f);
                     return true;
                 }
                 Location origen = player.getLocation();
                 Location destino = HomeManager.getHome(player, homeTp);
-                player.getWorld().spawnParticle(Particle.PORTAL, origen,30,0.5,1,0.5);
-                player.playSound(origen, Sound.ENTITY_ENDERMAN_TELEPORT,1f,1f);
+                player.getWorld().spawnParticle(Particle.PORTAL, origen, 30, 0.5, 1, 0.5);
+                player.playSound(origen, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
                 player.teleport(destino);
-                player.getWorld().spawnParticle(Particle.PORTAL, destino,30,0.5,1,0.5);
-                player.playSound(destino, Sound.ENTITY_ENDERMAN_TELEPORT,1f,1f);
+                player.getWorld().spawnParticle(Particle.PORTAL, destino, 30, 0.5, 1, 0.5);
+                player.playSound(destino, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
                 CooldownManager.setCooldown(player);
                 player.sendMessage("§aTeletransportado a '" + homeTp + "'!");
                 break;
+
             case "info":
                 if (args.length < 2) return false;
                 String homeInfo = args[1];
@@ -86,6 +90,7 @@ public class AHomeCommand implements CommandExecutor, Listener {
                 player.sendMessage("§7Mundo: " + loc.getWorld().getName());
                 player.sendMessage("§7Coordenadas: X=" + loc.getBlockX() + " Y=" + loc.getBlockY() + " Z=" + loc.getBlockZ());
                 break;
+
             default:
                 player.sendMessage("§cSubcomando desconocido!");
                 break;
@@ -104,8 +109,8 @@ public class AHomeCommand implements CommandExecutor, Listener {
 
         HomeManager.setHome(player, homeName, player.getLocation());
         player.sendMessage("§aHome '" + homeName + "' guardado correctamente!");
-        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP,1f,1f);
-        player.spawnParticle(Particle.VILLAGER_HAPPY, player.getLocation(),20,0.5,1,0.5);
+        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f);
+        player.spawnParticle(Particle.VILLAGER_HAPPY, player.getLocation(), 20, 0.5, 1, 0.5);
 
         awaitingName.remove(player.getUniqueId());
     }
