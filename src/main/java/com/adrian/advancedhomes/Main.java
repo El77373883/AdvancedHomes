9fa97a6d22887517d6dddc9acd055797bc764b83
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 21:26:41 -0600

    Update Main.java

--- src/main/java/com/adrian/advancedhomes/Main.java
+++ src/main/java/com/adrian/advancedhomes/Main.java
@@ -1,33 +1,15 @@
 package com.adrian.advancedhomes;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import com.adrian.advancedhomes.commands.AHomeCommand;
-import com.adrian.advancedhomes.commands.AdvancedHomeCommand;
-import org.bukkit.event.Listener;
 
-public class Main extends JavaPlugin implements Listener {
-
-    private static Main instance;
-
+public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-
-        getCommand("ahome").setExecutor(new AHomeCommand());
-        getCommand("advancedhome").setExecutor(new AdvancedHomeCommand());
-
-        getServer().getPluginManager().registerEvents(new AHomeCommand(), this);
-
-        getLogger().info("AdvancedHomes PRO habilitado!");
+        getLogger().info("AdvancedHomes habilitado!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("AdvancedHomes PRO deshabilitado!");
-    }
-
-    public static Main getInstance() {
-        return instance;
+        getLogger().info("AdvancedHomes deshabilitado!");
     }
 }
